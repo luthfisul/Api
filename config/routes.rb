f97a3294d29852671f53628dede3aace7c1c0ef9
exit <exit>
2020-12-31 12:15:39 +0700

    added pg to production

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :todos do
-    resources :items
-  end
+  #resources :todos do
+  # resources :items
+  #end
   namespace 'api' do
   	resources :sessions
   	resources :signup
