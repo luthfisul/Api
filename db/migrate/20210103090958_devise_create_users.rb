14e19e138e21d33812d040197b759048338f7561
exit <exit>
2021-02-16 12:37:25 +0700

    scheme

--- db/migrate/20210103090958_devise_create_users.rb
+++ db/migrate/20210103090958_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :username,             null: false, defaukt: ""
-      t.string :name,                 null: true, defaukt: ""
-      t.string :phone,                null: true, defaukt: ""
+      t.string :username,             null: false, default: ""
+      t.string :name,                 null: true, default: ""
+      t.string :phone,                null: true, default: ""
       t.string :email,                null: false, default: ""
-      t.string :imgUrl,                null: true, defaukt: ""
+      t.string :imgUrl,               null: true, default: ""
       t.string :encrypted_password,   null: false, default: ""
       t.string :authentication_token, null: false
 
